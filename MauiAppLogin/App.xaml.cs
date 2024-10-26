39b1ac510a4a89d29fbe2f28dc4c9a7c71d7a550
GutoMilare <gutomilare@hotmail.com>
2024-10-26 11:21:51 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiAppLogin
+﻿using System.Windows.Markup;
+
+namespace MauiAppLogin
 {
     public partial class App : Application
     {
@@ -7,6 +9,19 @@
             InitializeComponent();
 
             MainPage = new AppShell();
-        }
-    }
-}
+        } // Fecha método 1
+
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        } // Fecha método 2
+
+    } // Fecha classe
+
+} // Fecha namespace
